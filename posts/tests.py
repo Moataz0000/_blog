b22deb70925f620ad080fbb6bf2f62b46cfc2ebe
Moataz0000 <moatazfawzy150@gmail.com>
2025-03-31 20:49:30 +0200

    build the testing

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from posts.models import Post
 # Create your tests here.
+
+
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        self.post = Post.objects.create(
+            title='Test Title',
+            content='Test Content'
+        )
+
+    def test_post_creation(self):
+        post = Post.objects.get(id=1)
+        self.assertEqual(post.title, 'Test Title')
+        self.assertEqual(post.content, 'Test Content')
+        self.assertTrue(isinstance(post, Post))
